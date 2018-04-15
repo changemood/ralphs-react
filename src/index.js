e8b02e477dd0567cf8c0c919687347141c5680a9
naohiro-t <naohiro@xenon.io>
2018-04-15 20:54:44 +0900

    add store and routing to index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, compose, combineReducers } from 'redux';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import authReducer from './store/reducers/auth';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Redux devtool extention for development
+const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
+
+const rootReducer = combineReducers({
+  auth: authReducer
+});
+
+const store = createStore(rootReducer, composeEnhancers());
+
+const app = (
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+);
+
+ReactDOM.render( app, document.getElementById( 'root' ) );
 registerServiceWorker();
