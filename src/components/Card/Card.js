1b11dc3b30827c1ec29f2e8cdd0dd07fa576e21b
naohiro-t <naohiro@xenon.io>
2018-04-26 23:05:58 +0900

    add dropdown menu and onClick for Card component

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,29 +1,48 @@
 import React from 'react'
-import {Icon} from 'antd'
+import {Icon, Menu, Dropdown} from 'antd'
 
 import classes from './Card.module.css'
 
 const card = (props) => {
+  const onClick = function ({ key }) {
+    if (key === "1" ) {
+      console.log('Link to Board')
+    } else if ( key === "2" ) {
+      console.log('Stop review')
+    } else if ( key === "3") {
+      console.log('Open edit card')
+    }
+  };
+
+  const menu = (
+    <Menu onClick={onClick}>
+      <Menu.Item key="1">Go to board</Menu.Item>
+      <Menu.Item key="2">Stop Review</Menu.Item>
+      <Menu.Item key="3">Edit</Menu.Item>
+    </Menu>
+  );
+
   return (
     <div className={classes.Card}>
       <div className={classes.Main}>
         <div className={classes.Title}>
-          <p>Title</p>
-          {/* {props.title} */}
+          {props.title}
         </div>
         <div className={classes.Body}>
-          {/* {props.body} */}
+          {props.body}
         </div>
       </div>
       <div className={classes.Menu}>
-        <div className={classes.Button}>
+        <div className={classes.Button} onClick={props.reviewed}>
           <Icon type="like-o" style={{fontSize: 25 }}/>
         </div>
-        <div className={classes.Button}>
+        <div className={classes.Button} onClick={props.reviewAgain}>
           <Icon type="dislike-o" style={{fontSize: 25 }}/>
         </div>
         <div className={classes.DropDown}>
-          <Icon type="ellipsis" style={{fontSize: 25 }}/>
+          <Dropdown overlay={menu} placement="bottomCenter">
+              <Icon type="ellipsis" style={{fontSize: 25 }}/>
+          </Dropdown>
         </div>
       </div>
     </div>
