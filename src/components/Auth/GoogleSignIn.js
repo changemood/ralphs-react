5dbf22146567301b7604fc20c69ab1ea61c66db7
naohiro-t <naohiro@xenon.io>
2018-05-04 23:24:37 +0900

    add handleGoogleAuth to Login Signup

--- src/components/Auth/GoogleSignIn.js
+++ src/components/Auth/GoogleSignIn.js
@@ -2,11 +2,17 @@ import React from 'react'
 import {GoogleAPI, GoogleLogin} from 'react-google-oauth';
 
 const googleSignIn = (props) => {
-  console.log(process.env.REACT_APP_GOOGLE_CLIENT_ID)
   return (
     <GoogleAPI className="GoogleLogin" clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
       <div>
-        <GoogleLogin height="10" width="100%" backgroundColor="#4285f4" access="offline" scope="email profile" onLoginSuccess={props.responseGoogle} onFailure={props.responseGoogle}/>
+        <GoogleLogin height="10"
+                     width="100%"
+                     backgroundColor="#4285f4"
+                     access="offline"
+                     scope="email profile"
+                     onLoginSuccess={props.responseGoogle}
+                     onFailure={props.responseGoogle}
+                     text={props.text}/>
       </div>
     </GoogleAPI>
   )
