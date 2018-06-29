8dabf26a34d772ef417209ee23e237957795eda0
naohiro-t <naohiro@xenon.io>
2018-06-29 22:47:04 +0900

    avoid error when chrome doesn't have redux dev extention

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import authReducer from './store/reducers/auth';
 import reviewCardsReducer from './store/reducers/reviewCards';
 
 // Redux devtool extention for development
-const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
+const composeEnhancers = process.env.NODE_ENV === 'development' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
 
 const rootReducer = combineReducers({
   auth: authReducer,
