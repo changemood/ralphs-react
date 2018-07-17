43fdb23f6f5d27905a49743cc8b8c08c4498701f
naohiro-t <naohiro@xenon.io>
2018-07-17 21:33:50 +0900

    added reducer and action for boards

---
+++ src/store/actions/boards.js
@@ -0,0 +1,37 @@
+import axios from '../../utilities';
+
+import * as actionTypes from './actionTypes';
+
+export const fetchBoardsStart = () => {
+  return {
+    type: actionTypes.FETCH_BOARDS_START,
+  };
+};
+
+export const fetchBoardsSuccess = (boards) => {
+  return {
+    type: actionTypes.FETCH_BOARDS_SUCCESS,
+    boards: boards
+  };
+};
+
+export const fetchBoardsFail = (error) => {
+  return {
+    type: actionTypes.FETCH_BOARDS_FAIL,
+    error: error
+  };
+};
+
+export const fetchBoards = () => {
+  console.log("am I called...?")
+  return dispatch => {
+    dispatch(fetchBoardsStart());
+    axios.get('/v1/boards.json')
+    .then(response => {
+      dispatch(fetchBoardsSuccess(response.data));
+    })
+    .catch(err => {
+      dispatch(fetchBoardsFail(err.response.data));      
+    })
+  };
+}
---
+++ src/store/reducers/boards.js
@@ -0,0 +1,45 @@
+import * as actionTypes from '../actions/actionTypes';
+
+const initialState = {
+  error: null,
+  loading: false,
+  boards: []
+}
+
+const fetchBoardsStart = ( state, action ) => {
+  return {
+    ...state,
+    error: null,
+    loading: true
+  }
+};
+
+const fetchBoardsSuccess = (state, action) => {
+  console.log(action.boards)
+  console.log("called?")
+  return {
+    ...state,
+    boards: action.boards,
+    error: null,
+    loading: false
+  }
+};
+
+const fetchBoardsFail = (state, action) => {
+  return {
+    ...state,
+    error: action.error,
+    loading: false
+  }
+};
+
+const reducer = ( state = initialState, action ) => {
+  switch ( action.type ) {
+    case actionTypes.FETCH_BOARDS_START: return fetchBoardsStart(state, action);
+    case actionTypes.FETCH_BOARDS_SUCCESS: return fetchBoardsSuccess(state, action);
+    case actionTypes.FETCH_BOARDS_FAIL: return fetchBoardsFail(state, action);
+    default: return state;
+  }
+};
+
+export default reducer
