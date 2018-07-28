778b685e42d1ccaf5f093dd3b258e9cf0e0edf9d
naohiro-t <takahashi.naohiro1222@gmail.com>
2018-07-29 01:31:38 +0900

    update board state with onChange

--- src/containers/Boards/Board/Board.js
+++ src/containers/Boards/Board/Board.js
@@ -1,15 +1,24 @@
 import React,{Component} from 'react'
 import { connect } from 'react-redux'
+import { message } from 'antd';
+
 import * as actions from '../../../store/actions/index'
 import Aux from '../../../hoc/Aux/Aux'
 import CardsTree from './CardsTree/CardsTree'
 
 class Board extends Component {
+  // update board state. Only state not on api side!!
+  handleBoardNameChage = (event) => {
+    const newBoard = {...this.props.board,
+                      "name": event.target.value}
+    this.props.onSetBoard(newBoard)
+  }
+  
   render() {
     return (
       <Aux>
         <div>
-          <input type="text" value={this.props.board.name} onChange={this.handleChange} />
+          <input type="text" value={this.props.board.name} onChange={this.handleBoardNameChage} />
         </div>
         <CardsTree 
           board_id={this.props.match.params.id}
@@ -27,6 +36,8 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
   return {
+    onSetBoard: (data) => dispatch( actions.setBoard(data) ),
+    onUpdateBoard: (data) => dispatch( actions.updateBoard(data) )
   }
 }
 
