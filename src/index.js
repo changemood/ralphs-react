c4cd8d7d86c3cad365de9f99754fa8034c78ef0a
naohiro-t <takahashi.naohiro1222@gmail.com>
2018-08-25 12:25:10 +0900

    add redux-persist to keep state on refresh

--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore, compose, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import { persistStore, persistReducer } from 'redux-persist'
+import { PersistGate } from 'redux-persist/integration/react'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web and AsyncStorage for react-native
 
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import authReducer from './store/reducers/auth';
+import userReducer from './store/reducers/user';
 import reviewCardsReducer from './store/reducers/reviewCards';
 import cardsTreeReducer from './store/reducers/cardsTree';
 import manageCardReducer from './store/reducers/manageCard';
@@ -19,21 +23,33 @@ const composeEnhancers = process.env.NODE_ENV === 'development' && window.__REDU
 
 const rootReducer = combineReducers({
   auth: authReducer,
+  user: userReducer,
   reviewCards: reviewCardsReducer,
   cardsTree: cardsTreeReducer,
   manageCard: manageCardReducer,
   boards: boardsReducer,
 });
 
-const store = createStore(rootReducer, composeEnhancers(
+// To keep state with browser refresh. For now, we keep only user state.
+const persistConfig = {
+  key: 'user',
+  storage,
+  whitelist: ['user']
+}
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const store = createStore(persistedReducer, composeEnhancers(
   applyMiddleware(thunk)
 ));
+const persistor = persistStore(store)
 
 const app = (
   <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </PersistGate>
   </Provider>
 );
 
